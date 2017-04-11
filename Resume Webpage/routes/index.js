b1216c23bb12941f9fc281e0074856a6051b0297
TheFurnace <ferndq@ufl.edu>
2017-04-11 15:21:14 -0400

    first commit

---
+++ Resume Webpage/routes/index.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.sendFile('./public/index.html', { root: __dirname });
+});
+
+module.exports = router;
